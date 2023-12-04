070236b58234c04ccdc143a444f3cd268643a448
rtp.anantawat <rtp.anantawat@gmail.com>
2023-12-05 03:08:24 +0700

    🔧build:: add antd package

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { ConfigProvider } from "antd/lib";
+import type { ThemeConfig } from "antd/lib";
+
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
+
+const theme: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: "#0078FF",
+  },
+};
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -14,5 +24,9 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <ConfigProvider theme={theme}>
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
